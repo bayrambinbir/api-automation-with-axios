b238e73aa1cf0760512260ac9c64bd73d9d23119
bayrambinbir <38406467+bayrambinbir@users.noreply.github.com>
2023-09-30 12:24:06 -0500

    mochawesome reporting

--- api-tests/users/post_tests.js
+++ api-tests/users/post_tests.js
@@ -5,13 +5,12 @@ import { expect } from "chai";
 describe("POST tests for users", () => {
   let userId;
   it("POST user with randomly created data", async () => {
-    console.log("Authorization: " + qa.customConfig.headers.Authorization);
     const res = await axios.post(
       qa.baseUrl + "users",
       qa.randomUserData,
       qa.customConfig
     );
-    console.log(res.data.data);
+    //console.log(res.data.data);
     expect(res.status).to.be.eq(200);
     expect(res.data.code).to.be.eq(201);
     expect(res.data.data).to.not.null;
--- config/qa.js
+++ config/qa.js
@@ -43,8 +43,6 @@ export default {
       randomUserData,
       customConfig
     );
-    console.log("Random user's data: ");
-    console.log(res.data.data);
     return res.data.data.id;
   },
 };
